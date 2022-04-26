81bea4952be2197957bd4aa0d621e022ed5e3841
Júlia Furquim <furquim.julia@aluno.ifsp.edu.br>
2022-04-25 22:56:11 -0300

    first commit

---
+++ CalculadoraIMC/Pessoa.cs
@@ -0,0 +1,16 @@
+public class Pessoa
+{
+    public string nome { get; set; }
+    public float altura { get; set; }
+    public float peso { get; set; }
+
+    public Pessoa(string Nome, float Altura, float Peso)
+    {
+        nome = Nome;
+        altura = Altura;
+        peso = Peso;
+    }
+
+    
+
+}
---
+++ CalculadoraIMC/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace CalculadoraIMC
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+         
+            
+            Console.WriteLine("Insira seu nome:");
+            String nome = Console.ReadLine();
+            
+            Console.WriteLine("Insira sua altura(em metros):");
+            float altura = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Insira seu peso(em kg):");
+            float peso = Convert.ToSingle(Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa(nome, altura, peso);
+
+            double imc = peso / (altura * altura);
+
+       
+
+ if (imc < 18.5){
+  Console.WriteLine("VOCÊ ESTÁ ABAIXO DO PESO!!!");
+} 
+ else if (imc > 18.5 && imc < 24.9){
+  Console.WriteLine("VOCÊ ESTÁ NO SEU PESO IDEAL!!!");
+}
+ else if (imc > 25 && imc < 29.9){
+  Console.WriteLine("VOCÊ ESTÁ ACIMA DO PESO!!!");
+}
+ else if (imc > 30 && imc < 34.9){
+  Console.WriteLine("VOCÊ ESTÁ COM OBESIDADE GRAU I !!!");
+} 
+ else if (imc > 35 && imc < 39.9){
+  Console.WriteLine("VOCÊ ESTÁ COM OBESIDADE GRAU II !!!");
+}
+ else if (imc > 40){
+  Console.WriteLine("VOCÊ ESTÁ COM OBESIDADE GRAU III !!!");
+}
+    }
+    }
+}
+
+
